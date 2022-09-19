b86aec0bb9cfc6e22b12e9de8845982527fa5458
Johan Holmsteen <johanholmsteen@icloud.com>
2022-09-19 14:20:44 +0200

    Initial commit

---
+++ Regnemaskine.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Regnemaskine {
+
+        public static void main (String[] args){
+            double fnum;
+            double snum;
+            double ans;
+            char op;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter two numbers: ");
+            fnum = scan.nextDouble();
+            snum = scan.nextDouble();
+            System.out.print("\nEnter an operator (+, -, , /): ");
+            op = scan.next().charAt(0);
+            switch(op) {
+                case '+': ans = fnum + snum;
+                    break;
+                case '-': ans = fnum - snum;
+                    break;
+                case '*': ans = fnum * snum;
+                    break;
+                case '/': ans = fnum / snum;
+                    break;
+                default: System.out.print("Error! Enter correct operator");
+                    return;
+            }
+            System.out.print("\nThe result is given as follows:\n");
+            System.out.printf(fnum + " " + op + " " + snum + " = " + ans);
+
+        }
+}
+
